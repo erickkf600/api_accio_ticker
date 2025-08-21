1142e7f0b2e8cc73c7831992aafd95e0fde7227d
Erick Ferreira <erickkf600@gmail.com>
2025-08-21 18:43:16 -0300

    bypass caso não encontre

--- ticker_data.py
+++ ticker_data.py
@@ -1,6 +1,5 @@
 import yfinance as yf
 from cache_decorator import cache_memory
-from fetch_ticker_price_yahoo import fetch_ticker_price_yahoo
 
 @cache_memory(maxsize=100)
 def fetch_ticker_price(tickers):
@@ -11,15 +10,18 @@ def fetch_ticker_price(tickers):
 
     for ticker in tickers:
         ticker_completo = ticker + '.SA'
-
         try:
             # Obtém o preço atual do ticker
             current_price = tickers_data.tickers[ticker_completo].analyst_price_targets['current']
-            results.append({
-                'ticker': ticker,
-                'curPrc': round(current_price, 2)
-            })
+            if current_price is not None:
+                results.append({
+                    'ticker': ticker,
+                    'curPrc': round(current_price, 2)
+                })
+            else:     
+                print(f"{ticker_completo} não encontrado, pulando...")
         except Exception as e:
-            results = fetch_ticker_price_yahoo(tickers)
+            print(f"Erro ao buscar {ticker_completo}: {e}")
+            continue
 
     return results
